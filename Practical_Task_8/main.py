79fa8a9121678ff9e3f4525b2a98b5ea43ef30c2
Derafino <danya55555game@gmail.com>
2023-06-25 23:30:07 +0300

    init

---
+++ Practical_Task_8/main.py
@@ -0,0 +1,70 @@
+import random
+
+
+class ECPoint:
+    def __init__(self, x, y):
+        self.X = x
+        self.Y = y
+
+
+def BasePointGGet():
+    pass
+
+
+def ECPointGen():
+    pass
+
+
+def ECPointGen():
+    pass
+
+
+def IsOnCurveCheck():
+    pass
+
+
+def AddECPoints():
+    pass
+
+
+def DoubleECPoints():
+    pass
+
+
+def ScalarMult():
+    pass
+
+
+def ECPointToString():
+    pass
+
+
+def StringToECPoint():
+    pass
+
+
+def PrintECPoint():
+    pass
+
+
+def SetRandom(bits):
+    return random.randint(2 ** (bits - 1), 2 ** bits - 1)
+
+
+def main():
+    def SetRandom(bits):
+        return random.randint(2 ** (bits - 1), 2 ** bits - 1)
+
+    G = BasePointGGet()
+
+    k = SetRandom(256)
+    d = SetRandom(256)
+
+    H1 = ScalarMult(d, G)
+    H2 = ScalarMult(k, H1)
+    H3 = ScalarMult(k, G)
+    H4 = ScalarMult(d, H3)
+
+
+if __name__ == '__main__':
+    main()
