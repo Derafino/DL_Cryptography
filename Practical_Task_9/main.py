6fc56563d2f4ef290f930b0f0bdcd1b2c0217d1d
Derafino <danya55555game@gmail.com>
2023-07-05 18:19:19 +0300

    init

---
+++ Practical_Task_9/main.py
@@ -0,0 +1,45 @@
+import secrets
+from typing import Tuple
+
+from ecpy.curves import Curve
+from Practical_Task_8.main import get_base_point, scalar_mult, ECPoint
+
+curve = Curve.get_curve('secp256r1')
+base_point = get_base_point()
+
+
+def generate_keys() -> Tuple[int, ECPoint]:
+    n = curve.order
+    privKey = secrets.randbelow(n - 1) + 1
+    pubKey = scalar_mult(privKey, base_point)
+    return privKey, pubKey
+
+
+def get_shared_secret(my_private_key: int, their_public_key: ECPoint) -> int:
+    shared_point = scalar_mult(my_private_key, their_public_key)
+    return shared_point.X
+
+
+def ecdh_protocol() -> int:
+    privKey1, pubKey1 = generate_keys()
+    privKey2, pubKey2 = generate_keys()
+
+    shared_secret_1 = get_shared_secret(privKey1, pubKey2)
+    shared_secret_2 = get_shared_secret(privKey2, pubKey1)
+    print('privKey1:', privKey1)
+    print('pubKey1:', pubKey1)
+    print('privKey2:', privKey2)
+    print('pubKey2:', pubKey2)
+    print('shared_secret_1:', shared_secret_1)
+    print('shared_secret_2:', shared_secret_2)
+    assert shared_secret_1 == shared_secret_2
+    return shared_secret_1
+
+
+def main():
+    shared_secret_res = ecdh_protocol()
+    print(f'Shared secret: {shared_secret_res}')
+
+
+if __name__ == '__main__':
+    main()
