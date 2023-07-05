254459f9c6ee9bae151e53bf091a9b200c2f6bb2
Derafino <danya55555game@gmail.com>
2023-07-05 18:18:35 +0300

    fix

--- Practical_Task_8/main.py
+++ Practical_Task_8/main.py
@@ -1,90 +1,132 @@
-import random
-from cryptography.hazmat.primitives.asymmetric import ec
+import os
+from typing import Tuple
+
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import serialization
+from ecpy.curves import Curve, Point
+
+curve = Curve.get_curve('secp256r1')
 
 
 class ECPoint:
-    def __init__(self, x, y):
+    def __init__(self, x: int, y: int) -> None:
         self.X = x
         self.Y = y
 
 
-def BasePointGGet():
-    curve = ec.SECP256R1()
-    private_key = ec.generate_private_key(curve, default_backend())
-    public_key = private_key.public_key()
-    public_numbers = public_key.public_numbers()
-    return ECPoint(public_numbers.x, public_numbers.y)
+def generate_private_key() -> ec.EllipticCurvePrivateKey:
+    return ec.generate_private_key(ec.SECP256R1(), default_backend())
 
 
-def ECPointGen(x: int, y: int) -> ECPoint:
-    return ECPoint(x, y)
+def get_public_key(private_key: ec.EllipticCurvePrivateKey) -> ec.EllipticCurvePublicKey:
+    return private_key.public_key()
 
 
-def IsOnCurveCheck(a: ECPoint) -> bool:
-    curve = ec.SECP256R1()
-    public_numbers = ec.EllipticCurvePublicNumbers(a.X, a.Y, curve)
-    public_key = public_numbers.public_key(default_backend())
-
-    return public_key.public_numbers().x is not None
+def get_public_numbers(public_key: ec.EllipticCurvePublicKey) -> ec.EllipticCurvePublicNumbers:
+    return public_key.public_numbers()
 
 
-def AddECPoints(a: ECPoint, b: ECPoint) -> ECPoint:
-    if a.X == b.X and a.Y == b.Y:
-        s = ((3 * a.X * a.X) / (2 * a.Y))
-    else:
-        s = (b.Y - a.Y) / (b.X - a.X)
-    x3 = s * s - a.X - b.X
-    y3 = s * (a.X - x3) - a.Y
-
-    return ECPoint(x3, y3)
+def get_base_point() -> ECPoint:
+    private_key = generate_private_key()
+    public_key = get_public_key(private_key)
+    numbers = get_public_numbers(public_key)
+    return ECPoint(numbers.x, numbers.y)
 
 
-def DoubleECPoints(a: ECPoint) -> ECPoint:
-    s = (3 * a.X * a.X) / (2 * a.Y)
-    x3 = s * s - 2 * a.X
-    y3 = s * (a.X - x3) - a.Y
-
-    return ECPoint(x3, y3)
+def generate_ECPoint(x: int, y: int) -> ECPoint:
+    numbers = ec.EllipticCurvePublicNumbers(x, y, ec.SECP256R1())
+    return ECPoint(numbers.x, numbers.y)
 
 
-def ScalarMult(k: int, a: ECPoint) -> ECPoint:
-    result = None
-    while k > 0:
-        if k % 2 == 1:
-            result = AddECPoints(result, a)
-        a = DoubleECPoints(a)
-        k //= 2
-    return result
+def is_on_curve(point: ECPoint) -> bool:
+    try:
+        generate_ECPoint(point.X, point.Y)
+        return True
+    except:
+        return False
 
 
-def ECPointToString(point: ECPoint) -> str:
-    return f"X:{point.X}, Y:{point.Y}"
+def add_ECPoints(a: ECPoint, b: ECPoint) -> ECPoint:
+    point_a = Point(a.X, a.Y, curve)
+    point_b = Point(b.X, b.Y, curve)
+    result = point_a + point_b
+    return ECPoint(result.x, result.y)
 
 
-def StringToECPoint(s: str) -> ECPoint:
-    x, y = s.split(",")
-    return ECPoint(int(x), int(y))
+def double_ECPoint(a: ECPoint) -> ECPoint:
+    point_a = Point(a.X, a.Y, curve)
+    result = point_a + point_a
+    return ECPoint(result.x, result.y)
 
 
-def PrintECPoint(point: ECPoint) -> None:
-    print(ECPointToString(point))
+def scalar_mult(k: int, a: ECPoint) -> ECPoint:
+    point_a = Point(a.X, a.Y, curve)
+    result = k * point_a
+    return ECPoint(result.x, result.y)
 
 
-def SetRandom(bits):
-    return random.randint(2 ** (bits - 1), 2 ** bits - 1)
+def ECPoint_to_string(point: ECPoint) -> str:
+    numbers = ec.EllipticCurvePublicNumbers(point.X, point.Y, ec.SECP256R1())
+    public_key = numbers.public_key(default_backend())
+    pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+    return pem.decode('utf-8')
+
+
+def string_to_ECPoint(s: str) -> ECPoint:
+    pem = s.encode('utf-8')
+    public_key = serialization.load_pem_public_key(pem, default_backend())
+    numbers = public_key.public_numbers()
+    return ECPoint(numbers.x, numbers.y)
+
+
+def print_ECPoint(point: ECPoint) -> None:
+    print(f'point: ({point.X}, {point.Y})')
+
+
+def get_valid_point() -> Tuple[int, int]:
+    private_key = generate_private_key()
+    public_key = get_public_key(private_key)
+    numbers = get_public_numbers(public_key)
+    return numbers.x, numbers.y
+
+
+def set_random(bit_length: int) -> int:
+    return int.from_bytes(os.urandom(bit_length // 8), 'big')
+
+
+def is_equal(a: ECPoint, b: ECPoint) -> bool:
+    return a.X == b.X and a.Y == b.Y
 
 
 def main():
-    G = BasePointGGet()
-    print(G)
-    k = SetRandom(256)
-    d = SetRandom(256)
+    G = get_base_point()
+    print_ECPoint(G)
 
-    H1 = ScalarMult(d, G)
-    H2 = ScalarMult(k, H1)
-    H3 = ScalarMult(k, G)
-    H4 = ScalarMult(d, H3)
+    x, y = get_valid_point()
+    point = generate_ECPoint(x, y)
+    print_ECPoint(point)
+
+    print(is_on_curve(point))
+
+    s = ECPoint_to_string(point)
+    print(s)
+
+    point2 = string_to_ECPoint(s)
+    print_ECPoint(point2)
+    k = set_random(256)
+    d = set_random(256)
+
+    H1 = scalar_mult(d, G)
+    H2 = scalar_mult(k, H1)
+    H3 = scalar_mult(k, G)
+    H4 = scalar_mult(d, H3)
+
+    result = is_equal(H2, H4)
+    print(f'Equation result: {result}')
 
 
 if __name__ == '__main__':
